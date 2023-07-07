d79ba1dba227533d93094f5c9ba8b0e1c34bb46e
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-07-07 19:47:00 +0200

    Add repo repository

---
+++ src/main/java/com/example/repoviewer/Repository/RepoRepository.java
@@ -0,0 +1,61 @@
+package com.example.repoviewer.Repository;
+
+import com.example.repoviewer.Model.Repo;
+import com.example.repoviewer.Model.Response.RepoResponse;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class RepoRepository implements IRepoRepository {
+
+    @Override
+    public List<RepoResponse> getRepositoriesByUsername(String username) {
+        String apiUrl = "https://api.github.com/users/{username}/repos".replace("{username}", username);
+        RestTemplate restTemplate = new RestTemplate();
+        Repo[] repositories = restTemplate.getForObject(apiUrl, Repo[].class);
+        List<RepoResponse> repoResponses = new ArrayList<>();
+
+        if(repositories != null) {
+            for(Repo repository : repositories) {
+                repoResponses.add(new RepoResponse(
+                    repository.getRepositoryName(),
+                    getMostUsedLanguage(repository.getLanguagesUrl()),
+                    repository.getVisibility()
+                ));
+            }
+        }
+
+        return repoResponses;
+    }
+
+    @Override
+    public String getMostUsedLanguage(String languagesUrl) {
+        ParameterizedTypeReference<Map<String, Integer>> responseType = new ParameterizedTypeReference<>() {};
+        RequestEntity<Void> request = RequestEntity.get(languagesUrl).accept(MediaType.APPLICATION_JSON).build();
+        RestTemplate restTemplate = new RestTemplate();
+        Map<String, Integer> response = restTemplate.exchange(request, responseType).getBody();
+
+        String language = "";
+        int maxValue = Integer.MIN_VALUE;
+
+        if(response != null) {
+            for (Map.Entry<String, Integer> entry : response.entrySet()) {
+                if (entry.getValue() > maxValue) {
+                    maxValue = entry.getValue();
+                    language = entry.getKey();
+                }
+            }
+        }
+
+        return language;
+    }
+}
