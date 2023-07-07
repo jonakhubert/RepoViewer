6146ad942a493977956b11525d0f8b6fe52ae790
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-07-07 14:34:12 +0200

    Initial commit

---
+++ src/main/java/com/example/repoviewer/RepoViewerApplication.java
@@ -0,0 +1,13 @@
+package com.example.repoviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoViewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/repoviewer/RepoViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.repoviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
