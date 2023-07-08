17b59493c0488ab2b7e494c374814c709d910718
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-07-08 17:53:48 +0200

    Add github url as parameter

--- src/main/java/com/example/repoviewer/Models/Responses/RepoResponse.java
+++ src/main/java/com/example/repoviewer/Models/Responses/RepoResponse.java
@@ -5,5 +5,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public record RepoResponse(
         @JsonProperty("full_name") String repositoryName,
         @JsonProperty("language") String language,
-        @JsonProperty("visibility") String visibility
+        @JsonProperty("visibility") String visibility,
+        @JsonProperty("url") String url
 ) {}
