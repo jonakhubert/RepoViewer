9398c113e8c9b320bca7f1059d2cca8d29ec49d7
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-07-08 21:56:23 +0200

    Handle exceed rate limit exception

--- src/main/java/com/example/repoviewer/GlobalExceptionHandler.java
+++ src/main/java/com/example/repoviewer/GlobalExceptionHandler.java
@@ -1,20 +1,26 @@
-package com.example.repoviewer.Configs;
+package com.example.repoviewer;
 
-import com.example.repoviewer.Models.Exceptions.UnsupportedMediaTypeException;
 import com.example.repoviewer.Models.Responses.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpClientErrorException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(HttpClientErrorException.class)
+    @ExceptionHandler(HttpClientErrorException.NotFound.class)
     public ResponseEntity<ErrorResponse> handleUserNotFoundException() {
         return buildErrorResponse("User not found", HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(HttpClientErrorException.Forbidden.class)
+    public ResponseEntity<ErrorResponse> handleExceedRateLimit() {
+        return buildErrorResponse("Exceed rate limit", HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
@@ -22,9 +28,9 @@ public class GlobalExceptionHandler {
         return buildErrorResponse("Method not supported", HttpStatus.METHOD_NOT_ALLOWED);
     }
 
-    @ExceptionHandler(UnsupportedMediaTypeException.class)
-    public ResponseEntity<ErrorResponse> handleUnsupportedMediaTypeException(UnsupportedMediaTypeException ex) {
-        return buildErrorResponse(ex.getMessage(), HttpStatus.UNSUPPORTED_MEDIA_TYPE);
+    @ExceptionHandler(HttpClientErrorException.UnsupportedMediaType.class)
+    public ResponseEntity<ErrorResponse> handleBusinessException() {
+        return buildErrorResponse("Media type not acceptable", HttpStatus.UNSUPPORTED_MEDIA_TYPE);
     }
 
     private ResponseEntity<ErrorResponse> buildErrorResponse(String message, HttpStatus httpStatus) {
