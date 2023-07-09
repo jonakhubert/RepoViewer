c3ecd1b27913ad5ba5605458973f7eafc0bdfbda
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-07-09 14:35:59 +0200

    Create unsupported media type exception

---
+++ src/main/java/com/example/repoviewer/Exceptions/UnsupportedMediaTypeException.java
@@ -0,0 +1,7 @@
+package com.example.repoviewer.Exceptions;
+
+public class UnsupportedMediaTypeException extends RuntimeException {
+    public UnsupportedMediaTypeException(String message) {
+        super(message);
+    }
+}
