a8b55d0955815735b9c1c2281ee52323b0674b69
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-07-09 14:37:45 +0200

    Add if statement for unsupported media type

--- src/main/java/com/example/repoviewer/Services/RepoService.java
+++ src/main/java/com/example/repoviewer/Services/RepoService.java
@@ -1,6 +1,7 @@
 package com.example.repoviewer.Services;
 
-import com.example.repoviewer.Models.Responses.RepoResponse;
+import com.example.repoviewer.Exceptions.UnsupportedMediaTypeException;
+import com.example.repoviewer.Responses.RepoResponse;
 import com.example.repoviewer.Repositories.RepoRepository;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +15,10 @@ public class RepoService {
         this.repoRepository = repoRepository;
     }
 
-    public List<RepoResponse> getRepositories(String username) {
+    public List<RepoResponse> getRepositories(String username, String acceptHeader) {
+        if(acceptHeader.equals("application/xml"))
+            throw new UnsupportedMediaTypeException("Media not supported: " + acceptHeader);
+
         return repoRepository.getRepositoriesByUsername(username);
     }
 }
