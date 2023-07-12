54ffec0d16fe63e1fe3bfd53b63eb3e6daca80d7
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-07-12 15:41:31 +0200

    Add bean for rest template

--- src/main/java/com/example/repoviewer/RepoViewerApplication.java
+++ src/main/java/com/example/repoviewer/RepoViewerApplication.java
@@ -2,11 +2,18 @@ package com.example.repoviewer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class RepoViewerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RepoViewerApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 }
